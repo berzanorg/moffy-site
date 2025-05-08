da492eda47f5fb57bdc10e5c16359963d250ef0f
berzan <berzan@monda.exchange>
2025-05-08 20:42:06 +0900

    edit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,11 @@
   <h1 class="text-4xl cursor-default">Deploying sadness...</h1>
   <img src="/moffy.avif" alt="moffy" class="w-full max-w-80" />
   <div class="flex gap-8">
-    <a href="https://kuru.io/" class="text-3xl underline hover:text-green-400" target="_blank">Kuru</a>
+    <a
+      href="https://www.kuru.io/trade/0xfc518989aea6cb1b0d0cb3c16045dd96eb4f6b05"
+      class="text-3xl underline hover:text-green-400"
+      target="_blank">Kuru</a
+    >
     <a href="https://x.com/Moffysad" class="text-3xl underline hover:text-blue-300" target="_blank">@Moffysad</a>
   </div>
 </main>
