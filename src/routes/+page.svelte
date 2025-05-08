412c5f38ddfee043c17b23d9b7cd270eac4445c5
berzan <berzan@monda.exchange>
2025-05-08 20:14:32 +0900

    edit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="flex flex-col text-center p-8 items-center justify-center flex-1 gap-6">
+  <h1 class="text-4xl cursor-default">Deploying sadness...</h1>
+  <img src="/moffy.avif" alt="moffy" class="w-full max-w-80" />
+  <div class="flex gap-8">
+    <a href="https://kuru.io/" class="text-3xl underline hover:text-green-400" target="_blank">Kuru</a>
+    <a href="https://x.com/Moffysad" class="text-3xl underline hover:text-blue-300" target="_blank">@Moffysad</a>
+  </div>
+</main>
